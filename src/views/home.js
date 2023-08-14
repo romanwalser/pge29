ab734d1338c34d08fd728ad3eb6e50d5c1fb8843
romanwalser <142220074+romanwalser@users.noreply.github.com>
2023-08-14 12:21:12 +0200

    Commit from TeleportHQ

---
+++ src/views/home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container">
+      <Helmet>
+        <title>Trained Low Wolf</title>
+        <meta property="og:title" content="Trained Low Wolf" />
+      </Helmet>
+      <a href="https://pl1.kamadeta.com/" className="home-link">
+        <img alt="image" src="/523533-400h.jpg" className="home-image" />
+      </a>
+    </div>
+  )
+}
+
+export default Home

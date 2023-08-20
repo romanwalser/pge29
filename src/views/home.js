7c6305f7bc3c9d10070da26065a8ebc15b957c82
romanwalser <142220074+romanwalser@users.noreply.github.com>
2023-08-20 18:59:02 +0200

    Update home.js

--- src/views/home.js
+++ src/views/home.js
@@ -11,7 +11,7 @@ const Home = (props) => {
         <title>Trained Low Wolf</title>
         <meta property="og:title" content="Trained Low Wolf" />
       </Helmet>
-      <a href="https://pl1.kamadeta.com/" className="home-link">
+      <a href="https://pl1.meorkasoib.live/" className="home-link">
         <img alt="image" src="/523533-400h.jpg" className="home-image" />
       </a>
     </div>

e13f6ce81e07606ceb82290c2cd680b524685d84
romanwalser <142220074+romanwalser@users.noreply.github.com>
2023-08-20 19:24:00 +0200

    Commit from TeleportHQ

--- src/views/home.js
+++ src/views/home.js
@@ -11,7 +11,12 @@ const Home = (props) => {
         <title>Trained Low Wolf</title>
         <meta property="og:title" content="Trained Low Wolf" />
       </Helmet>
-      <a href="https://pl1.meorkasoib.live/" className="home-link">
+      <a
+        href="https://pl1.kalkuala.store/"
+        target="_blank"
+        rel="noreferrer noopener"
+        className="home-link"
+      >
         <img alt="image" src="/523533-400h.jpg" className="home-image" />
       </a>
     </div>
